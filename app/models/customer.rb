1bb0ce3a7dbf280a2c91ea6ba7c64da07ac09788
ryoyaa <ryoya76lima@gmail.com>
2018-05-03 17:18:34 +0900

    devise関連の画面修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,8 +6,6 @@ class Customer < ApplicationRecord
   validates :given_name, presence: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-  validates :company_id, presence: true
-  validates :post_id, presence: true
 
 
   def full_name
