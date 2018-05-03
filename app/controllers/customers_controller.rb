860b588a735b15230a26037a2e169a45bd02ae08
ryoyaa <ryoya76lima@gmail.com>
2018-05-03 13:54:25 +0900

    一覧機能作成

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,7 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
   def index
+    @customers = Customer.page(params[:page])
   end
 
   def new
