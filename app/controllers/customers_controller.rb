4daa428f43ce1925d4c569126184193e57cee0dc
ryoyaa <ryoya76lima@gmail.com>
2018-05-03 12:03:45 +0900

    更新処理終了

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,5 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
   def index
   end
 
@@ -8,18 +9,25 @@ class CustomersController < ApplicationController
 
   def create
     @customer = Customer.new(customer_params)
-    @customer.save
-    redirect_to @customer
+    if @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
   end
 
   def show
-    @customer = Customer.find(params[:id])
   end
 
   def destroy
@@ -29,5 +37,9 @@ class CustomersController < ApplicationController
 
   def customer_params
     params.require(:customer).permit(:family_name, :given_name, :email)
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
   end
 end
